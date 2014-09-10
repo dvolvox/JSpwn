47f3b17e24ac761c6e42d5223ca78ab793b7b0c6
Etraud123 <duarteetraud@gmail.com>
2014-09-10 17:17:49 +0100

    JSON output added

--- jspwn.js
+++ jspwn.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 eval(fs.readFileSync(__dirname + '/common/analyzer.js') + '');
 eval(fs.readFileSync(__dirname + '/common/engine.js') + '');
 
-var argv = require('optimist').usage('Usage: $node jspwn.js -t [path/to/app]').demand(['t']).argv;
+var argv = require('optimist').usage('Usage: $node jspwn.js -t [path/to/app] -j [for json output]').demand(['t']).argv;
 
 var results = {};
 var dir= argv.t;
@@ -14,6 +14,7 @@ var res = "";
 var htmlcontent = "<html><head><title>Output scanner</title></head><body>Data:"+ Date() + "</br><table>";
 var time_scan = 0;
 var cnt = 0;
+var output = "";
 
 //Recursive Function to sumarize all sub folder and files.
 function walk(dir) {
@@ -30,23 +31,26 @@ function walk(dir) {
 
 var files = walk(dir);
 console.log("$ SYSTEM: Files: " + files);
-for( var i = 0; i < files.length; i++){
-	var data = fs.readFileSync(files[i], "utf8");
-	adata.push({"id":i,"nome":files[i],"code":data});
+for( var cnt = 0; cnt < files.length; cnt++){
+	console.log(cnt);
+	var data = fs.readFileSync(files[cnt], "utf8");
+	var start = new Date().getTime();
+	var iss = scan(data);
+	var end = new Date().getTime();
+	adata.push({"id":i,"nome":files[cnt],"time":end-start,"issues":iss});
 }
 
-console.log(adata);
-
-for(var ct = 0 ; ct < adata.length; ct++){
-	htmlcontent = htmlcontent.concat(scan(adata[ct]));
-	//console.log(adata);  //socket.emit('init', {data: data});
-}
-
-htmlcontent = htmlcontent.concat("</table><br><strong>Time Scan(ms):</strong>"+time_scan+"</body></html>");
+//console.log(adata);
 var d = new Date();
-var path = __dirname + '/output' + d.getDate() + '-' + d.getMonth() + '.html';
-buffer = new Buffer(htmlcontent);
-fs.open(path, 'w', function(err, fd) {
+var path = __dirname + '/output' + d.getDate() + '-' + d.getMonth();
+
+
+if(argv.o){
+	//printout JSON format
+	path = path.concat(".json");
+	buffer = new Buffer(JSON.stringify(adata));
+	console.log("Data to be written:" + adata);
+	fs.open(path, 'w', function(err, fd) {
     if (err) {
         throw 'error opening file: ' + err;
     } else {
@@ -57,31 +61,50 @@ fs.open(path, 'w', function(err, fd) {
             })
         });
     }
-});
+	});
+}
+else{
+	p_html(adata);
+}
 
-//console.log(htmlcontent);
+//Save HTML format
+function p_html(jsonobj){
+	path = path.concat(".html");
+	for(var xnt = 0; xnt < jsonobj.length; xnt++){
+		var tmp_a = jsonobj[xnt].issues;
+		htmlcontent = htmlcontent.concat("<br><table></br><tr><td><strong>["+jsonobj[xnt].id+"]</strong></td><td><strong>"+jsonobj[xnt].nome+"<strong></td><td>TimeScan:["+jsonobj[xnt].time+"]</br>");	
+		for(var dnt = 0; dnt < tmp_a.length ; dnt++){
+			console.log(JSON.stringify(jsonobj[xnt].issues[1]));
 
+			htmlcontent = htmlcontent.concat("<tr><td><span style=\"color:"+ jsonobj[xnt].issues[dnt].css + "\">"+ jsonobj[xnt].issues[dnt].nome + "</span></td><td>"+ jsonobj[xnt].issues[dnt].line +"</td><td>"+ jsonobj[xnt].issues[dnt].code +"</td></tr>");
+		}
+	}
+	buffer = new Buffer(htmlcontent);
+	fs.open(path, 'w', function(err, fd) {
+    if (err) {
+        throw 'error opening file: ' + err;
+    } else {
+        fs.write(fd, buffer, 0, buffer.length, null, function(err) {
+            if (err) throw 'error writing file: ' + err;
+            fs.close(fd, function() {
+                console.log('$ SYSTEM: file written [' + path + ']');
+            })
+        });
+    }
+	});
+
+}
 
 //Execute JSPrime function
 function scan(data){
 	//console.log(time);
-	code = data.code;
-	var ret = "</br><table><tr><td><strong>"+data.nome+"</strong></td><td><strong>["+data.id+"]<strong></td></br>"
-	var start = new Date().getTime();
-	var ret_2 = analyze(code); 
-	var end = new Date().getTime();
-	var obj_json = {};
-	time_scan+=end-start;
-	obj_json = {"ScanTime":end-start, "nfic":data.nome,"issues":[]};
-
-	var file_a = code.split('\n');
-	//console.log(code);
-
-	//console.log();
-	//console.log('RESULTADO DA TRAFULHICE: ' + ret_2);
+	var ret = analyze(data); 
+	var file_a = data.split('\n');
+	
 	var tmp_array = [];
-	for(var i = 0; i < ret_2.length; i++){
-		var color = ret_2[i][1];
+
+	for(var i = 0; i < ret.length; i++){
+		var color = ret[i][1];
 		var tmp_i = "";
 		var css = "";
 		if ( color == 'orange'){
@@ -108,47 +131,12 @@ function scan(data){
 			tmp_i = "Active Sink";
 			css = "#ff6666";
 	    }   
-		tmp_array.push(new_issue(tmp_i,ret_2[i][0],file_a[ret_2[i][0]-1],css));
-/*		console.log('Resultado numero:'+ i);
-		console.log('linha:' + ret_2[i][0]);
-		console.log('Tipo:' + ret_2[i][1]);
-		console.log('Codigo:' + file_a[ret_2[i][0]-1]);*/
+		tmp_array.push(new_issue(tmp_i,ret[i][0],file_a[ret[i][0]-1],css));
 	}
-	obj_json.issues = tmp_array;
-	//console.log("OBJJSON" + obj_json.issues);
-
-	//htmlcontent = htmlcontent.concat(JSON.stringify(obj_json));
-	for(var x = 0; x < obj_json.issues.length; x++){
-		console.log(obj_json.issues[x]);
-		ret = ret.concat("<tr><td><span style=\"color:"+ obj_json.issues[x].css + "\">"+ obj_json.issues[x].nome + "</span></td><td>"+ obj_json.issues[x].line +"</td><td>"+ obj_json.issues[x].code +"</td></tr>");
-	}
-	return ret;
+	return tmp_array;
 }
 
 //create a new data struture
 function new_issue(name,line,code,css){
 	return {"nome": name, "line":line, "code": code, "css":css};
 }
-
-/*
-fs.readdir(dir,function(err,files){
-    if (err) throw err;
-    var c=0;
-    files.forEach(function(file){
-        c++;
-        fs.readFile(dir+'/'+file,'utf-8',function(err,html){
-            if (err) throw err;
-            data.push({"id":c,"fic":file, "code":html});
-            if (0===--c) {
-                for(var i = 0 ; i < data.length; i++){
-                	htmlcontent = htmlcontent.concat(scan(data[i]));
-                	console.log(data);  //socket.emit('init', {data: data});
-
-                }
-                htmlcontent = htmlcontent.concat("</table></body></html>");
-				write2file(htmlcontent);
-				console.log(htmlcontent);
-            }
-        });
-    });
-});*/
